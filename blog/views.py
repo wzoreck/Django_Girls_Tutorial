95c198af9bea0054ad4e60d557b13220fb65c1d6
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-06-14 20:31:48 -0300

    Adicionada funcionalidade de publicar Post rascunho

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') 
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -17,7 +17,6 @@ def post_new(request):
          if form.is_valid():
              post = form.save(commit=False)
              post.author = request.user
-             post.published_date = timezone.now()
              post.save()
              return redirect('post_detail', pk=post.pk)
      else:
@@ -31,9 +30,17 @@ def post_edit(request, pk):
          if form.is_valid():
              post = form.save(commit=False)
              post.author = request.user
-             post.published_date = timezone.now()
              post.save()
              return redirect('post_detail', pk=post.pk)
      else:
          form = PostForm(instance=post)
      return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
