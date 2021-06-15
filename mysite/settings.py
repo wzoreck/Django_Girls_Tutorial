fe91fe45800542015d1ab42fc78f66bc3325886b
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-06-15 14:55:27 -0300

    Mundaca para banco de dados MySQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'NOME_DB',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'USUARIO',
+        'PASSWORD': 'SENHA',
     }
 }
 
